05f698896aafad944c1458b9b96caebc01a66adb
Thejana-A <thejanaakmeemana@gmail.com>
2023-07-22 13:48:26 +0530

    reversed solved & timeout in master

--- app/src/main/java/com/example/androidnativeapp1/translator/OngoingSessionView.kt
+++ app/src/main/java/com/example/androidnativeapp1/translator/OngoingSessionView.kt
@@ -1,6 +1,5 @@
 package com.example.androidnativeapp1.translator
 
-import android.app.Dialog
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -10,7 +9,6 @@ import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import com.example.androidnativeapp1.R
-import com.example.androidnativeapp1.home.Home
 import com.example.androidnativeapp1.utilities.CameraActivity
 
 
@@ -23,41 +21,11 @@ class OngoingSessionView : AppCompatActivity() {
 
         val leaveSession: Button = findViewById(R.id.leaveSession)
         leaveSession.setOnClickListener {
-            cancelSessionDialog()
-            //startActivity(Intent(this, CancelSessionLayout::class.java))
+            startActivity(Intent(this, CancelSessionLayout::class.java))
         }
 
         CameraActivity.start(this)
 
-    }
-
-    private fun cancelSessionDialog() {
-        val dialog = Dialog(this)
-        dialog.setContentView(R.layout.cancel_session_layout)
-
-        val continueSessionButton = dialog.findViewById<Button>(R.id.continueSessionButton)
-        continueSessionButton.setOnClickListener {
-            dialog.dismiss()
-        }
-
-        val cancelSessionButton = dialog.findViewById<Button>(R.id.cancelSessionButton)
-        cancelSessionButton.setOnClickListener {
-            dialog.dismiss()
-            sessionCompletedDialog()
-        }
-        dialog.show()
-    }
-
-    private fun sessionCompletedDialog() {
-        val dialog = Dialog(this)
-        dialog.setContentView(R.layout.session_completed_layout)
-
-        val backToHomeButton = dialog.findViewById<Button>(R.id.backToHomeButton)
-        backToHomeButton.setOnClickListener {
-            dialog.dismiss()
-            startActivity(Intent(this, Home::class.java))
-        }
-        dialog.show()
     }
 
     /** Check if this device has a camera */
